d63d18ca2f6924472df0b8b008cdd161fa5d89b2
WyattBlue <wyattblue@auto-editor.com>
2023-10-30 00:35:16 -0400

    Try disabling amr and x265

--- scripts/build-ffmpeg.py
+++ scripts/build-ffmpeg.py
@@ -220,11 +220,11 @@ if not os.path.exists(output_tarball):
             name="ogg",
             source_url="http://downloads.xiph.org/releases/ogg/libogg-1.3.5.tar.gz",
         ),
-        Package(
-            name="opencore-amr",
-            source_url="http://deb.debian.org/debian/pool/main/o/opencore-amr/opencore-amr_0.1.5.orig.tar.gz",
-            build_parallel=plat != "Windows",
-        ),
+        # Package(
+        #     name="opencore-amr",
+        #     source_url="http://deb.debian.org/debian/pool/main/o/opencore-amr/opencore-amr_0.1.5.orig.tar.gz",
+        #     build_parallel=plat != "Windows",
+        # ),
         Package(
             name="openjpeg",
             requires=["cmake"],
@@ -283,24 +283,24 @@ if not os.path.exists(output_tarball):
             fflags="--enable-libx264",
             gpl=True,
         ),
-        Package(
-            name="x265",
-            requires=["cmake"],
-            source_url="https://bitbucket.org/multicoreware/x265_git/downloads/x265_3.5.tar.gz",
-            build_system="cmake",
-            source_dir="source",
-            fflags="--enable-libx265",
-            gpl=True,
-        ),
-        Package(
-            name="xvid",
-            requires=["nasm"],
-            source_url="https://downloads.xvid.com/downloads/xvidcore-1.3.7.tar.gz",
-            source_dir="build/generic",
-            build_dir="build/generic",
-            fflags="--enable-libxvid",
-            gpl=True,
-        ),
+        # Package(
+        #     name="x265",
+        #     requires=["cmake"],
+        #     source_url="https://bitbucket.org/multicoreware/x265_git/downloads/x265_3.5.tar.gz",
+        #     build_system="cmake",
+        #     source_dir="source",
+        #     fflags="--enable-libx265",
+        #     gpl=True,
+        # ),
+        # Package(
+        #     name="xvid",
+        #     requires=["nasm"],
+        #     source_url="https://downloads.xvid.com/downloads/xvidcore-1.3.7.tar.gz",
+        #     source_dir="build/generic",
+        #     build_dir="build/generic",
+        #     fflags="--enable-libxvid",
+        #     gpl=True,
+        # ),
     ]
 
     for package in package_groups:
@@ -314,8 +314,8 @@ if not os.path.exists(output_tarball):
         "--enable-gpl",
         "--enable-version3",
 
-        "--enable-libopencore-amrnb",
-        "--enable-libopencore-amrwb",
+        #"--enable-libopencore-amrnb",
+        #"--enable-libopencore-amrwb",
         "--enable-libxcb" if plat == "Linux" else "--disable-libxcb",
         "--enable-lzma",
         "--enable-zlib",
