53cfa8ece7d89d916304941e1996d48f3fdbaec7
WyattBlue <wyattblue@auto-editor.com>
2024-03-02 14:25:19 -0500

    Add back amr, upgrade some libs

--- scripts/build-ffmpeg.py
+++ scripts/build-ffmpeg.py
@@ -76,7 +76,7 @@ if not os.path.exists(output_tarball):
     package_groups = [
         Package(
             name="xz",
-            source_url="https://github.com/tukaani-project/xz/releases/download/v5.4.4/xz-5.4.4.tar.xz",
+            source_url="https://github.com/tukaani-project/xz/releases/download/v5.6.0/xz-5.6.0.tar.xz",
             build_arguments=[
                 "--disable-doc",
                 "--disable-lzma-links",
@@ -141,7 +141,7 @@ if not os.path.exists(output_tarball):
         package_groups += [
             Package(
                 name="unistring",
-                source_url="https://ftp.gnu.org/gnu/libunistring/libunistring-1.1.tar.gz",
+                source_url="https://ftp.gnu.org/gnu/libunistring/libunistring-1.2.tar.gz",
             ),
             Package(
                 name="gmp",
@@ -222,11 +222,11 @@ if not os.path.exists(output_tarball):
             name="ogg",
             source_url="http://downloads.xiph.org/releases/ogg/libogg-1.3.5.tar.gz",
         ),
-        # Package(
-        #     name="opencore-amr",
-        #     source_url="http://deb.debian.org/debian/pool/main/o/opencore-amr/opencore-amr_0.1.5.orig.tar.gz",
-        #     build_parallel=plat != "Windows",
-        # ),
+        Package(
+            name="opencore-amr",
+            source_url="http://deb.debian.org/debian/pool/main/o/opencore-amr/opencore-amr_0.1.5.orig.tar.gz",
+            build_parallel=plat != "Windows",
+        ),
         Package(
             name="openjpeg",
             requires=["cmake"],
@@ -301,8 +301,8 @@ if not os.path.exists(output_tarball):
     openh264 = Package(
         name="openh264",
         requires=["meson", "nasm", "ninja"],
-        source_filename="openh264-2.4.0.tar.gz",
-        source_url="https://github.com/cisco/openh264/archive/refs/tags/v2.4.0.tar.gz",
+        source_filename="openh264-2.4.1.tar.gz",
+        source_url="https://github.com/cisco/openh264/archive/refs/tags/v2.4.1.tar.gz",
         build_system="meson",
     )
 
